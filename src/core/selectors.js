ee11294b81980dadd57ad2ab35e7af46e6cc81d8
kaungpyae1200 <kaungpyae120045@gmail.com>
2023-12-02 23:11:08 +0630

    update

--- src/core/selectors.js
+++ src/core/selectors.js
@@ -2,4 +2,6 @@ export const app = document.querySelector("#app");
 export const categoryGroup = document.querySelector("#categoryGroup");
 export const productGroup = document.querySelector("#productGroup");
 export const categoryTemplate = document.querySelector("#categoryTemplate");
-export const productTemplate = document.querySelector("#productTemplate")
+export const productTemplate = document.querySelector("#productTemplate");
+export const fillStarTemplate = document.querySelector("#fillStarTemplate");
+export const outlineStarTemplate = document.querySelector("#outlineStarTemplate");
